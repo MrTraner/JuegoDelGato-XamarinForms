7ebbc93e462d3f93f9ea8cf4493103a6adf93a6c
MrTraner <juanmauricio.ochoa@hotmail.com>
2019-03-31 17:44:01 -0600

    Versión inicial

---
+++ JuegoDelGato/JuegoDelGato/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JuegoDelGato
+{
+    public partial class MainPage : ContentPage
+    {
+        private int turno = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCasilla_Clicked(object sender, EventArgs e)
+        {
+            if (turno == 0)
+            {
+                ((Button)sender).Text = "X";
+                turno = 1;
+            }
+            else
+            {
+                ((Button)sender).Text = "O";
+                turno = 0;
+            }
+        }
+    }
+}
